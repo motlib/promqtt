e3dee56429ecae9452a16919abe8c57926d668fb
Andreas Schroeder <andreas@a-netz.de>
2019-07-01 14:07:47 +0800

    Start adding config options

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
+import argparse
 import json
 import logging
 import os
 import signal
 import sys
-import traceback
-import time
 
 import paho.mqtt.client as mqtt
 
@@ -16,19 +15,67 @@ def sigterm_handler(signum, stack_frame):
     logging.info('Terminating promqtt. Bye!')
     sys.exit(0)
 
+
+# configuration with default values
+_config = {
+    'http_interface': '127.0.0.1',
+    'http_port': 8000,
+    'mqtt_broker': 'mqtt',
+    'mqtt_port': 1883,
+}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Tasmota MQTT to Prometheus exporter.")
+
+    parser.add_argument(
+        '-i', '--http-interface',
+        help='Set the listening interface and port for the HTTP server, e.g. 127.0.0.1:8000',
+        required=False,
+        default=None)
+
+    parser.add_argument(
+        '-m', '--mqtt-broker',
+        help='MQTT broker hostname and port, e.g mqtt:1883',
+        required=False)
+
+    return parser.parse_args()
     
+
+def eval_args(args):
+    if args.http_interface is not None:
+        iface, port = args.http_interface.split(':')
+        _config['http_interface'] = iface
+        _config['http_port'] = int(port)
+
+    if args.mqtt_broker:
+        host, port = args.mqtt_broker.split(':')
+        _config['mqtt_broker'] = host
+        _config['mqtt_port'] = int(port)
+        
+
 def main():
-    logfmt = '[%(levelname)s] (%(threadName)-10s) %(message)s'
+    logfmt = '[%(levelname)s] (%(threadName)s) %(message)s'
     logging.basicConfig(level=logging.DEBUG, format=logfmt)
     logging.info('Starting promqtt.')    
 
     signal.signal(signal.SIGTERM, sigterm_handler)
-    
-    pe = PrometheusExporter(http_iface='0.0.0.0', http_port=8000)
+
+    args = parse_args()
+    eval_args(args)
+
+    pe = PrometheusExporter(
+        http_iface=_config['http_interface'],
+        http_port=_config['http_port'])
     pe.start_server_thread()
 
+    msg = 'Connecting to MQTT broker at {0}:{1}.'
+    logging.info(msg.format(_config['mqtt_broker'], _config['mqtt_port']))
     mqttc = mqtt.Client()
-    mqttc.connect('npi2', 1883)
+    mqttc.connect(
+        host=_config['mqtt_broker'],
+        port=_config['mqtt_port'])
     
     tmc = TasmotaMQTTClient(mqttc, pe, prefix=['tasmota'])
 
