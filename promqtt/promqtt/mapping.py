6985446ebce8037cbef6efe10315b8088a9a83c0
Andreas Schroeder <andreas@a-netz.de>
2021-10-23 16:47:02 +0200

    Improve error handling for processing messages. Add comments.

--- promqtt/promqtt/mapping.py
+++ promqtt/promqtt/mapping.py
@@ -7,11 +7,14 @@ logger = logging.getLogger(__name__)
 
 
 class Mapping():
-    '''A mapping takes data from a message received from MQTT, extracts a value
-    and possibly also values and submits the results to the prometheus
-    exporter.'''
+    '''A mapping takes data from a message received from MQTT, extracts a value and
+    possibly also label values and submits the results to the prometheus
+    exporter.
 
-    def __init__(self, promexp, value_exp, label_exps, metric):
+    '''
+
+    def __init__(self, promexp, type_name, value_exp, label_exps, metric): # pylint: disable=too-many-arguments
+        self._type_name = type_name
         self._promexp = promexp
         self._value_exp = value_exp
         self._label_exps = label_exps
@@ -21,23 +24,56 @@ class Mapping():
     def handle_msg_data(self, msg):
         '''Handle a message received from MQTT'''
 
+        # calculate value
+
         try:
             value = self._value_exp.format(
                 msg=msg)
-        except KeyError:
-            logger.error(f"Cannot apply value expression '{self._value_exp}'. Message: {msg}")
+        except Exception as ex: # pylint: disable=broad-except
+            # We only print a debug log, as we want the message handling to be
+            # fault tolerant. Message contents sometimes change over time and
+            # not every member is available all the time. To issues are to be
+            # expected
+            logger.debug(
+                f"{self}: Cannot apply value expression '{self._value_exp}' "
+                f"for message {msg}: {ex}")
             return
 
+
         # calculate labels
-        bind_labels = {
-            k.format(msg=msg):
-            v.format(msg=msg)
-            for k,v in self._label_exps.items()}
+
+        labels = {}
+        label_fault = False
+
+        for name_exp, value_exp in self._label_exps.items():
+            try:
+                label_name = name_exp.format(msg=msg)
+                label_value = value_exp.format(msg=msg)
+
+                labels[label_name] = label_value
+            except Exception as ex: # pylint: disable=broad-except
+
+                # We only print a debug log, as we want the message handling to
+                # be fault tolerant. Message contents sometimes change over time
+                # and not every member is available all the time. To issues are
+                # to be expected
+                logger.debug(
+                    f"{self}: Cannot apply label expression '{self._value_exp}' "
+                    f"for message {msg}: {ex}")
+
+                label_fault = True
+
+        # if at least one label had a fault, we return
+        if label_fault:
+            return
+
+
+        # Hand over metric data to prometheus exporter
 
         self._promexp.set(
             name=self._metric,
-            labels=bind_labels,
+            labels=labels,
             value=value)
 
     def __str__(self):
-        return f'Mapping(metric={self._metric})'
+        return f'Mapping(type={self._type_name}, metric={self._metric})'
