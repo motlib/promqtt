0ce56e95da66597e6c44cc65b48a8433d829730e
Andreas Schroeder <andreas@a-netz.de>
2021-10-30 10:00:24 +0200

    Automatically include version info in container

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,17 +9,25 @@ then
     exit 1
 fi
 
+VERSION="$(date "+%Y%m%d-%H%M%S")-$(git describe --always)"
+
 if [ ${ENV} == "prod" ]
 then
-    VERSION="promqtt:$(date "+%Y%m%d-%H%M%S")-$(git describe --always)"
-    echo "Tagging production image '${VERSION}'."
-    TAG_VERSION="--tag ${VERSION}"
+    echo "Tagging production image 'promqtt:${VERSION}'."
+    TAG_VERSION="--tag promqtt:${VERSION}"
+
 else
     TAG_VERSION=""
 fi
+
+# Update version metadata
+sed -i -e "s/^__version__ = '.*'/__version__ = '${VERSION}'/" promqtt/__version__.py
 
 docker build \
        --build-arg ENV=${ENV} \
        --tag promqtt:${ENV} \
        ${TAG_VERSION} \
        .
+
+# discard change to version metadata
+git checkout promqtt/__version__.py
